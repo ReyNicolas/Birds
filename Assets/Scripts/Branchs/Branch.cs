591df1f135f34e7b8243b288bffc210bcd610a45
ReyNicolas <rey.nicolas.92@gmail.com>
2023-11-23 11:26:33 -0300

    Added points to collor logic

--- Assets/Scripts/Branchs/Branch.cs
+++ Assets/Scripts/Branchs/Branch.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +8,8 @@ public class Branch : MonoBehaviour
 {
     [SerializeField] List<Zone> zones;
 
+    public static Action<int, Color> OnPointsToColor; 
+
     private void OnEnable()
     {
         zones.ForEach(zone =>  zone.OnBirdEnter += (_ => CheckAllBirdsInBranchSameColor()));        
@@ -14,23 +17,34 @@ public class Branch : MonoBehaviour
     private void OnDisable()
     {
         zones.ForEach(zone => zone.OnBirdEnter -= (_ => CheckAllBirdsInBranchSameColor()));
-    }
-
-    
+    }    
     public Zone TryGiveMeCloseZone(Vector2 point)
     {
         var freeZones = zones.Where(z => z.myBird == null);
 
         return (freeZones.Count() > 0)
-            ? freeZones.OrderBy(z=> Vector2.Distance(z.transform.position,point)).First() 
+            ? freeZones
+                .OrderBy(z=> Vector2.Distance(z.transform.position,point))
+                .First() 
             : null;
     }
-
     void CheckAllBirdsInBranchSameColor()
     {
-        if (zones.Any(zone => zone.myBird == null))
+        if (ThereAreEmptyZones())
             return;
-        if (zones.GroupBy(zone => zone.myBird.GetColor()).Count() == 1)
-            Debug.Log("cumple");
+        if (AllZoneBirdsAreTheSameColor())
+            OnPointsToColor?.Invoke(GetDistinctsBirdsInZoneCount(), GetFirstZoneBirdColor());
     }
+
+    Color GetFirstZoneBirdColor() 
+        => zones.First().myBird.GetColor();
+
+    int GetDistinctsBirdsInZoneCount() 
+        => zones.Select(zone => zone.myBird).Distinct().Count();
+
+    bool AllZoneBirdsAreTheSameColor() 
+        => zones.GroupBy(zone => zone.myBird.GetColor()).Count() == 1;
+
+    bool ThereAreEmptyZones()
+        => zones.Any(zone => zone.myBird == null);
 }
